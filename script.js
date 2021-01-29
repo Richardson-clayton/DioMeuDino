47d8458ce03809d4a48bf7ecf77fe7b02cf129af
Richardson Clayton <70027084+Richardson-clayton@users.noreply.github.com>
2021-01-28 23:19:31 -0300

    estudos code

---
+++ script.js
@@ -0,0 +1,39 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.PlnoFundo');
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+      if (!isJumping) {
+        jump();
+      }
+    }
+  }
+
+  function jump() {
+    isJumping = true;
+  
+    let upInterval = setInterval(() => {
+      if (position >= 150) {
+        // Descendo
+        clearInterval(upInterval);
+  
+        let downInterval = setInterval(() => {
+          if (position <= 0) {
+            clearInterval(downInterval);
+            isJumping = false;
+          } else {
+            position -= 20;
+            dino.style.bottom = position + 'px';
+          }
+        }, 20);
+      } else {
+        // Subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+      }
+    }, 20);
+  }
